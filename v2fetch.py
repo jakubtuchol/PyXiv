f211ac62ae99c76aa6120ec17d1bbd805610339c
Ubuntu <ubuntu@jtuchol.novalocal>
2013-12-02 16:20:47 -0600

    Almost finished with second script implementation. Just need to test it several times

--- v2fetch.py
+++ v2fetch.py
@@ -9,13 +9,13 @@ nDataBytes, nRawbytes, nRecoveries, maxRecoveries = 0, 0, 0, 3
 
 def getFile(fetchBase, command, verbose=1, sleepTime=0):
     global nRecoveries, nDataBytes, nRawBytes
+    # sleep if server commands function to timeout
     if sleepTime:
         time.sleep(sleepTime)
     remoteAddr = fetchBase + command
-    
+    # verbose option used primarily when getting HTTP error from server
     if verbose:
-        print "\r", "getFile ... '%s'" % remoteAddr[-90:]
-        
+        print "\r", "getFile ... '%s'" % remoteAddr[-90:]        
     try:
         remoteData = urllib2.urlopen(remoteAddr).read()
     except urllib2.HTTPError, exValue:
@@ -31,44 +31,55 @@ def getFile(fetchBase, command, verbose=1, sleepTime=0):
             return getFile(fetchBase, command, 1, 60)
         return
     nRawBytes += len(remoteData)
-
-    # check for data compression
+    # check for data compression and decompress if is present
     try: 
         remoteData = zlib.decompressobj().decompress(remoteData)
     except:
         pass
     nDataBytes += len(remoteData)
-    
+    # check and catch any errors
     oaiError = re.search('<error *code=\"([^"]*)">(.*)</error>', remoteData)
     if oaiError:
         print "OAIERROR: code=%s '%s'" % (oaiError.group(1), oaiError.group(2))
     else:
         return remoteData
 
-fetchBase = 'http://export.arxiv.org/oai2/request?verb=ListRecords'
-fetchCommand = '&metadataPrefix=Arxiv'
-outputFileName = 'metadata.xml'
-print "Writing records to %s from archive %s" % (outputFileName, fetchBase)
+if __name__ == "__main__":
+    # url base for what our data
+    fetchBase = 'http://export.arxiv.org/oai2/request?verb=ListRecords'
+    # our URL option-- will be replaced by resumption token in next iteration
+    fetchCommand = '&metadataPrefix=Arxiv'
+    # name for our output file
+    outputFileName = 'metadata.xml'
+    print "Writing records to %s from archive %s" % (outputFileName, fetchBase)
 
-outputFile = codecs.lookup('utf-8')[-1](file(outputFileName, 'wb'))
-outputFile.write('<repository xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/" \
- xmlns:dc="http://purl.org/dc/elements/1.1/" \
- xmlns:xsi="http://www.w3.org/2001/XMLSchema-instanc">\n')
+    # creating file for data output
+    outputFile = codecs.lookup('utf-8')[-1](file(outputFileName, 'wb'))
+    # creating XML wrapper around our data
+    outputFile.write('<repository xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/" \ xmlns:dc="http://purl.org/dc/elements/1.1/" \ xmlns:xsi="http://www.w3.org/2001/XMLSchema-instanc">\n')
 
-data = getFile(fetchBase, fetchCommand)
+    # getting our initial data
+    data = getFile(fetchBase, fetchCommand)
+    # initializing count of records that we're getting
+    recordCount = 0
+    
+    # will loop while we're still getting data from the server
+    while data:
+        events = xml.dom.pulldom.parseString(data)
+        for (event, node) in events:
+            if event == "START_ELEMENT" and node.tagName == 'record':
+                events.expandNode(node)
+                node.writexml(outputFile)
+                recordCount += 1
+                # parse resumption token from output
+                resToken = re.search('<resumptionToken[^>]*>(.*)</resumptionToken>', data)
+                # if we don't have the resumption token, we assume that we're done
+                if not resToken:
+                    break
+                # call OAI API using reumption token
+                data = getFile(fetchBase, "&resumptionToken=%s" % mo.group(1))
 
-recordCount = 0
-
-while data:
-    events = xml.dom.pulldom.parseString(data)
-    for (event, node) in events:
-        if event == "START_ELEMENT" and node.tagName == 'record':
-            events.expandNode(node)
-            node.writexml(outputFile)
-            recordCount += 1
-        resToken = re.search('<resumptionToken[^>]*>(.*)</resumptionToken>', data)
-        if not resToken:
-            break
-        data = getFile(fetchBase, "&resumptionToken=%s" % mo.group(1)
-
-outputFile.write('\n</repository>\n'), outputFile.close()
+    # wrap and close our output file
+    outputFile.write('\n</repository>\n'), outputFile.close()
+    print "\nRead %d bytes (%.2f compression)" % (nDataBytes, float(nDataBytes) / nRawBytes)
+    print "Wrote out %d records" % recordCount
